632de199e4bbd03347e1a3b4f5247bdae65c9675
Javier Esmerado <esmerado.vela.javier@gmail.com>
2023-10-30 23:10:47 +0100

    First commit

---
+++ data_collection.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct 30 22:06:55 2023
+
+@author: javieresmeradovela
+"""
+
+import glassdoor_scraper as gs
+import pandas as pd
+
+path = "/Users/javieresmeradovela/Documents/dev/conda/ds_salary/chromedriver"
+
+df = gs.get_jobs('data scientist', 15, False, path, 15)
